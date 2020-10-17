a17bf9a94bc8fe2ed1e352017c52d19a57175480
5h1ro <rajamodus7073@gmail.com>
2020-10-17 23:24:32 +0700

    Ini adalah pesan commit pertama anda

---
+++ app/src/main/java/com/pnm/praktikummingguke7/MainActivity.java
@@ -0,0 +1,99 @@
+package com.pnm.praktikummingguke7;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MainActivity extends AppCompatActivity {
+    EditText txtNPM,txtPassword;
+    String sNPM,sPassword;
+    String url = "https://pajuts.000webhostapp.com/login.php";
+
+    public static String md5(String message){
+        String digest = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hash = md.digest(message.getBytes("UTF-8"));
+            StringBuilder sb = new StringBuilder(2*hash.length);
+            for(byte b : hash){
+                sb.append(String.format("%02x", b&0xff));
+            }
+            digest = sb.toString();
+        } catch (UnsupportedEncodingException ex){
+            Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchAlgorithmException ex){
+            Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, null, ex);
+        } return digest;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtNPM = findViewById(R.id.txtNPM);
+        txtPassword = findViewById(R.id.txtPassword);
+    }
+
+    public void Login(View view) {
+        if(txtNPM.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show();
+        } else if(txtPassword.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Password", Toast.LENGTH_SHORT).show();
+        } else {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Please Wait..");
+            progressDialog.show();
+            sNPM = txtNPM.getText().toString().trim();
+            sPassword = md5(txtPassword.getText().toString().trim());
+            StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    progressDialog.dismiss();
+                    if(response.equalsIgnoreCase("Selamat Datang")){
+                        txtNPM.setText("");
+                        txtPassword.setText("");
+                        startActivity(new Intent(getApplicationContext(),MainAplikasi.class));
+                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            },new Response.ErrorListener(){
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this, error.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<String, String>();
+                    params.put("npm",sNPM);
+                    params.put("password",sPassword);
+                    return params;
+                }
+            };
+
+            RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+            requestQueue.add(request);
+        }
+    }
+}
---
+++ app/src/main/java/com/pnm/praktikummingguke7/MainAplikasi.java
@@ -0,0 +1,14 @@
+package com.pnm.praktikummingguke7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainAplikasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_aplikasi);
+    }
+}
