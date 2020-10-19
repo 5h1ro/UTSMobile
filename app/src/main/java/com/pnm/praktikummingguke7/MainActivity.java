28b354539cdb84dfb19dfa2d7e3ef0ccefeac5a9
5h1ro <rajamodus7073@gmail.com>
2020-10-19 11:58:24 +0700

    Menambah registrasi

--- app/src/main/java/com/pnm/praktikummingguke7/MainActivity.java
+++ app/src/main/java/com/pnm/praktikummingguke7/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
@@ -47,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         txtNPM = findViewById(R.id.txtNPM);
         txtPassword = findViewById(R.id.txtPassword);
@@ -95,5 +97,10 @@ public class MainActivity extends AppCompatActivity {
             RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
             requestQueue.add(request);
         }
+    }
+
+    public void Regis(View view) {
+        startActivity(new Intent(getApplicationContext(),MainRegistrasi.class));
+        finish();
     }
 }
--- app/src/main/java/com/pnm/praktikummingguke7/MainAplikasi.java
+++ app/src/main/java/com/pnm/praktikummingguke7/MainAplikasi.java
@@ -3,11 +3,13 @@ package com.pnm.praktikummingguke7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainAplikasi extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_aplikasi);
     }
